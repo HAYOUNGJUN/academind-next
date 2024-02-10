7fb7cdfd5e6c0f130f7f500f3e7cf7d70efd74bd
Youngjun Ha <dudwnsk011@gmail.com>
2024-02-10 23:13:19 +0900

    Add Suspense component

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -11,7 +11,7 @@ export type CommentWithAuthor = Comment & {
 
 export const fetchCommentsByPostId = cache(
   (postId: string): Promise<CommentWithAuthor[]> => {
-    console.log('Making a query!');
+    // console.log('Making a query!');
 
     return db.comment.findMany({
       where: { postId },
